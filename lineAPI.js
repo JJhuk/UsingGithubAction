8b9fca7ad4da1e69287bc5ad49b1387badf25fb3
juhyuklee <juhyuklee@shiftup.co.kr>
2020-07-17 14:45:10 +0900

    LineAPI

---
+++ lineAPI.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const TARGET_URL='https://notify-api.line.me/api/notify'
+const TOKEN = '7AxVSvUUOsFki4Lo7y3wRpzYyUyJTnODpPr4973cLOr'
+
+request.post({
+    url:TARGET_URL,
+    headers: {
+        'Authorization': `Bearer ${TOKEN}`
+    },
+    form: {
+        message: '안녕하세요. Line Notify 테스트입니다.'
+    }
+}, (error, response,body) => {
+    console.log(body);
+})
