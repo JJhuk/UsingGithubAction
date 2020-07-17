02720c9491426af798679eba0ad624498a4169ef
juhyuklee <juhyuklee@shiftup.co.kr>
2020-07-17 11:41:57 +0900

    fff

---
+++ out/helloweb.js
@@ -0,0 +1,8 @@
+var A = /** @class */ (function () {
+    function A(name, numFeet) {
+        this.feet = numFeet;
+        this.name = name;
+    }
+    return A;
+}());
+//# sourceMappingURL=helloweb.js.map
