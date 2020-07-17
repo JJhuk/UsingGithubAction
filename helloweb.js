659903a059ee0be4b69f7529abfa7bbb69c1ffda
juhyuklee <juhyuklee@shiftup.co.kr>
2020-07-17 12:48:37 +0900

    dfdf

--- helloweb.js
+++ helloweb.js
@@ -1,10 +1,1 @@
-"use strict";
-exports.__esModule = true;
-var data = require("./c01.json");
-var skins = data.skins;
-var attachments = [];
-// for(let i = 0;i<skins.length;i++) {
-//     if(skins[i].attachments !== null) {
-//         for(let j = 0;j<skins[i].attachments)
-//     }
-// }
+console.log("Hello World");
